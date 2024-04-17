ab98aefdc61a2c7a6e439bdfdc3164c4afd01e9c
Ann He <annxhe@gmail.com>
2024-04-17 17:03:48 +0100

    add many transformer features

--- attention.py
+++ attention.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 import math
 import numpy as np
 import copy
+from torch.autograd import Variable
+import torch.nn.functional as F
 
 
 def clones(module, N):
@@ -72,6 +74,160 @@ class MultiHeadedAttention(nn.Module):
 		out = self.dropout(out)
 		return out
 
+
+class FeedForwardNetwork(nn.Module):
+	def __init__(self,d_model,d_ff,dropout=0.1):
+		super(FeedForwardNetwork, self).__init__()
+		self.d_model = d_model
+		self.d_ff = d_ff
+		self.w_1 = nn.Linear(d_model,d_ff)
+		self.w_2 = nn.Linear(d_ff, d_model)
+		self.dropout = nn.Dropout(dropout)
+
+	def forward(self,x):
+		return self.w_2(self.dropout(F.ReLU(self.w_1(x))))
+
+class Embeddings(nn.Module):
+	def __init__(self, d_model, vocab):
+		super(Embeddings, self).__init__()
+		self.lut = nn.Embedding(vocab, d_model)
+
+	def forward(self, x):
+		return self.lut(x) * math.sqrt(self.d_model)
+
+class PositionalEncoding(nn.Module):
+	def __init__(self,
+				emb_size: int,
+				dropout: float,
+				maxlen: int = 5000
+		):
+		super(PositionalEncoding, self).__init__()
+		self.embedding_matrix = torch.zeros((maxlen, emb_size))
+		pos = torch.arange(0,maxlen).reshape(maxlen,1) # for broadcasting
+		denominator = torch.exp(-torch.arange(0,emb_size,2)*math.log(10000)/emb_size)
+		self.embedding_matrix[:, 0::2] = torch.sin(pos * denomiator)
+		self.embedding_matrix[:, 1::2] = torch.cos(pos * denominator)
+		self.pos_embedding = pos_embedding.unsqueeze(0)
+
+		self.dropout = nn.Dropout(dropout)
+		self.register_buffer('pos_embedding', pos_embedding)
+
+	def forward(self, token_embedding: Tensor):
+		return self.dropout(token_embedding + self.pos_embedding[:token_embedding.size(0),:])
+
+class Generator(nn.Module):
+	"Linear + softmax generation step."
+	def __init__(self, d_model, vocab):
+		super(Generator, self).__init__()
+		self.proj = nn.Linear(d_model, vocab)
+
+	def forward(self, x):
+		return F.log_softmax(self.proj(x), dim=-1)
+
+class LayerNorm(nn.Module):
+	def __init__(self, size, eps=1e-6):
+		super(LayerNorm, self).__init__()
+		self.a_2 = nn.Parameter(torch.ones(size))
+		self.b_2 = nn.Parameter(torch.zeros(size))
+		self.eps = eps
+
+	def forward(self, x):
+		mean = x.mean(-1, keepdim=True)
+		std = x.std(-1, keepdim=True)
+		return self.a_2 * (x-mean) / (std + self.eps) + self.b_2 
+
+class SublayerConnection(nn.Module):
+	"""
+	Residual connection followed by layer norm, where you can 
+	pass in the sublayer function, eg mha or ff
+	"""
+	def __init__(self, size, dropout):
+		super(SublayerConnection, self).__init__()
+		self.norm = LayerNorm(size)
+		self.dropout = nn.Dropout(dropout)
+
+	def forward(self, x, sublayer):
+		return self.norm(x + self.dropout(sublayer(x)))
+
+class EncoderLayer(nn.Module):
+	def __init__(self, size, self_attn, feed_forward, dropout):
+		super(EncoderLayer, self).__init__()
+		self.self_attn = self_attn
+		self.feed_forward = feed_forward
+		self.sublayer = clones(SublayerConnection(size,dropout), 2)
+		self.size = size # d_model
+
+	def forward(self, x, mask):
+		x = self.sublayer[0](x, lambda x: self.self_attn(x,x,x,mask))
+		return self.sublayer[1](x, feed_foward)
+
+class Encoder(nn.Module):
+	def __init__(self, layer, N):
+		super(Encoder, self).__init__()
+		self.layers = clones(layer, N)
+		self.norm = LayerNorm(layer.size) # d_model # is this necessary?
+
+	def forward(self, x, mask):
+		for layer in self.layers:
+			x = layer(x, mask)
+		return self.norm(x)
+
+
+class DecoderLayer(nn.Module):
+	def __init__(self, size, self_attn, feed_forward, dropout):
+		super(EncoderLayer, self).__init__()
+		self.self_attn = self_attn
+		self.feed_forward = feed_forward
+		self.sublayer = clones(SublayerConnection(size,dropout), 3)
+		self.size = size # d_model
+
+	def forward(self, x, encoder_outputs, source_mask, target_mask):
+		m = encoder_outputs
+		x = self.sublayer[0](x, lambda x: self.self_attn(x,x,x,target_mask))
+		x = self.sublyaer[1](x, lambda x: self.self_attn(x,m,m,source_mask))
+		return self.sublayer[2](x, feed_foward)
+
+class Decoder(nn.Module):
+	def __init__(self, layer, N):
+		super(Encoder, self).__init__()
+		self.layers = clones(layer, N)
+		self.norm = LayerNorm(layer.size) # d_model # is this necessary?
+
+	def forward(self, x, encoder_outputs, source_mask, target_mask):
+		for layer in self.layers:
+			x = layer(x, encoder_outputs, source_mask, target_mask)
+		return self.norm(x)
+
+
+class Batch:
+    "Object for holding a batch of data with mask during training."
+    def __init__(self, src, trg=None, pad=0):
+        self.src = src
+        self.src_mask = (src != pad).unsqueeze(-2)
+        if trg is not None:
+            self.trg = trg[:, :-1]
+            self.trg_y = trg[:, 1:]
+            self.trg_mask = \
+                self.make_std_mask(self.trg, pad)
+            self.ntokens = (self.trg_y != pad).data.sum()
+    
+    @staticmethod
+    def make_std_mask(tgt, pad):
+        "Create a mask to hide padding and future words."
+        tgt_mask = (tgt != pad).unsqueeze(-2)
+        tgt_mask = tgt_mask & Variable(
+            subsequent_mask(tgt.size(-1)).type_as(tgt_mask.data))
+        return tgt_mask
+
+
+def data_gen(V, batch_size, nbatches, d_model=10):
+    "Generate random data for a src-tgt copy task."
+    for i in range(nbatches):
+        data = torch.from_numpy(np.random.randint(1, V, size=(batch_size, d_model)))
+        data[:, 0] = 1
+        src = Variable(data, requires_grad=False)
+        tgt = Variable(data, requires_grad=False)
+        yield Batch(src, tgt, 0)
 
 
 def main():
