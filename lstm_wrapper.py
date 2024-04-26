01390b48289f8887612928a3d687d1bc93ac56c7
Ann He <annxhe@gmail.com>
2024-04-26 10:34:54 +0100

    add skeleton code for training lstm

--- lstm_wrapper.py
+++ lstm_wrapper.py
@@ -6,10 +6,6 @@ from torch.autograd import Variable
 import torch.nn.functional as F
 import math
 
-def clones(module, N):
-    "Produces N identical layers."
-    return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
-
 
 class LSTM(nn.Module):
 	def __init__(self, vocab_size, hidden_size):
@@ -18,6 +14,8 @@ class LSTM(nn.Module):
 		self.hidden = nn.zeros(1,hidden_size)
 		self.cell_state = nn.zeros(1,hidden_size)
 		# other init variables
+		self.final_linear = nn.Linear(hidden_size, vocab_size)
+		self.log_softmax = nn.LogSoftmax()
 
 	def forward(self, x):
 		# x is size N, L, D_vocab
@@ -28,3 +26,27 @@ class LSTM(nn.Module):
 			outputs.append(hidden[:,None,:])
 		# want output of size N, L, D_hidden
 		outputs = torch.cat(outputs, dim=1)
+		outputs = self.final_linear(outputs)
+		outputs = self.log_softmax(outputs,dim=2)
+		return outputs
+
+def train(model, criterion, optimizer, train_dataloader, vocab_size, hidden_size, n_epochs):
+	for epoch in n_epochs:
+		loss = 0
+		for i, (x,y) in enumerate(train_dataloader): # make sure dataloader is shuffled
+			# y = toTensorFormat(y)
+			# x = toTensorFormat(x)
+			# N x L x D
+			optimizer.zero_grad()
+			y_pred = model(x)
+			loss += criterion(y_pred, y)
+			loss.backward()
+			optimizer.step()
+
+
+def main():
+	model = LSTM(vocab_size, hidden_size)
+	criterion = nn.NLLLoss()
+	optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+
+
